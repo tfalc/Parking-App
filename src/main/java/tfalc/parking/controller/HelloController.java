9b9932787d658e276f1a7051a298563650bef9af
Thiago F <thiago.falcao86@gmail.com>
2022-11-17 00:36:37 -0300

    Create HelloController.java

---
+++ src/main/java/tfalc/parking/controller/HelloController.java
@@ -0,0 +1,13 @@
+package tfalc.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Welcome to the parking application!";
+    }
+}
