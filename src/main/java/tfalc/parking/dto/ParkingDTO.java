ba5f2c293e805b3b79753b040cb6de622c00dbd6
Thiago F <thiago.falcao86@gmail.com>
2022-11-17 01:05:04 -0300

    Update ParkingDTO.java

--- src/main/java/tfalc/parking/dto/ParkingDTO.java
+++ src/main/java/tfalc/parking/dto/ParkingDTO.java
@@ -1,10 +1,12 @@
 package tfalc.parking.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParkingDTO {
 
     private String id;
