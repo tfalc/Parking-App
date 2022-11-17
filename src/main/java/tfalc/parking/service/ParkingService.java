5cac77ee20b04357d896bb02e620b50219f9b71c
Thiago F <thiago.falcao86@gmail.com>
2022-11-17 01:05:08 -0300

    Update ParkingService.java

--- src/main/java/tfalc/parking/service/ParkingService.java
+++ src/main/java/tfalc/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package tfalc.parking.service;
 import org.springframework.stereotype.Service;
 import tfalc.parking.model.Parking;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "KOF-9828", "RJ", "VW GOL", "White");
+        Parking parkingBus = new Parking(id1, "KMS-1231", "SP", "Bus", "Yellow");
         parkingMap.put(id, parking);
+        parkingMap.put(id1,parkingBus);
     }
 
     public List<Parking> findAll(){
@@ -25,4 +29,15 @@ public class ParkingService {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking createParking(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }
