73942e781f1b9719fb0b5a41ffecf29cfaca40c6
Thiago F <thiago.falcao86@gmail.com>
2022-11-27 01:46:43 -0300

    Update ParkingControllerIntegrationTest.java

--- src/test/java/tfalc/parking/controller/ParkingControllerIntegrationTest.java
+++ src/test/java/tfalc/parking/controller/ParkingControllerIntegrationTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import tfalc.parking.dto.ParkingCreateDTO;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ParkingControllerIntegrationTest {
@@ -21,6 +24,7 @@ class ParkingControllerIntegrationTest {
 
     @Test
     void whenFindAllThenCheckResult() {
+
         RestAssured.given()
                 .when()
                 .get("/parking/parked")
@@ -30,5 +34,22 @@ class ParkingControllerIntegrationTest {
 
     @Test
     void createParked() {
+
+        var createDTO = new ParkingCreateDTO();
+
+        createDTO.setColor("Yellow");
+        createDTO.setLicense("Placa");
+        createDTO.setModel("Etios");
+        createDTO.setState("Novo");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/createParked")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("license", Matchers.equalTo("Placa"))
+                .body("color", Matchers.equalTo("Yellow"));
     }
 }
